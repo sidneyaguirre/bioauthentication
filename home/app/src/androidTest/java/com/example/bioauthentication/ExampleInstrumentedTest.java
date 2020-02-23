9eb35382e02d4178064448687d3e5d90df4df9be
Jaime Jimenez Serrano <jaimejimenezisi@gmail.com>
2020-02-23 12:30:37 -0500

    home with material design

--- home/app/src/androidTest/java/com/example/bioauthentication/ExampleInstrumentedTest.java
+++ home/app/src/androidTest/java/com/example/bioauthentication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.home;
+package com.example.bioauthentication;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.home", appContext.getPackageName());
+        assertEquals("com.example.bioauthentication", appContext.getPackageName());
     }
 }
