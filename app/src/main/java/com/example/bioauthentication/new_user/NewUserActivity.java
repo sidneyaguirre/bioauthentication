50d566c8c0d194b82a8a5113bd37dde775c36d1c
sidneyaguirre <sidney.aguirrec@gmail.com>
2020-02-29 12:13:11 -0500

    new user screen

---
+++ app/src/main/java/com/example/bioauthentication/new_user/NewUserActivity.java
@@ -0,0 +1,18 @@
+package com.example.bioauthentication.new_user;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewUserActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
