8855a48e0fe0f6f2a91a9afb6109273dbfd5e1ac
Jaime Jimenez Serrano <jaimejimenezisi@gmail.com>
2020-02-29 16:34:38 -0500

    adding users

---
+++ app/src/main/java/com/example/bioauthentication/user/User.java
@@ -0,0 +1,23 @@
+package com.example.bioauthentication.user;
+
+public class User {
+
+    private int uid;
+    private String name;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
