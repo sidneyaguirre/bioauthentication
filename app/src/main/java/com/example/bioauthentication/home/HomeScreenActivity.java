efca25349209a8b16d529c2eb84190786849ae00
Jaime Jimenez Serrano <jaimejimenezisi@gmail.com>
2020-02-29 13:50:03 -0500

    adding new user intent

--- app/src/main/java/com/example/bioauthentication/home/HomeScreenActivity.java
+++ app/src/main/java/com/example/bioauthentication/home/HomeScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.bioauthentication.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bioauthentication.R;
+import com.example.bioauthentication.user.NewUserActivity;
 import com.example.bioauthentication.pattern.InputPasswordActivity;
 import com.example.bioauthentication.pin.LockActivity;
 
@@ -18,7 +21,7 @@ import com.example.bioauthentication.pin.LockActivity;
 public class HomeScreenActivity extends AppCompatActivity {
 
     private AutoCompleteTextView usersDropDown, testTypesDropDown;
-    private Button pinBtn, patternBtn;
+    private Button pinBtn, patternBtn ,addUserBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,7 @@ public class HomeScreenActivity extends AppCompatActivity {
         testTypesDropDown = findViewById(R.id.filled_dropdown_test_types);
         pinBtn = findViewById(R.id.pin_button);
         patternBtn = findViewById(R.id.pattern_button);
+        addUserBtn = findViewById(R.id.add_user_button);
 
         ArrayAdapter<CharSequence> usersAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.users_array, R.layout.dropdown_menu_popup_item);
         ArrayAdapter<CharSequence> testTypesAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.test_types_array, R.layout.dropdown_menu_popup_item);
@@ -39,6 +43,13 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         patternBtn.setOnClickListener(utils.setClickListener(getApplicationContext(),InputPasswordActivity.class, usersDropDown, testTypesDropDown));
         pinBtn.setOnClickListener(utils.setClickListener(getApplicationContext(), LockActivity.class, usersDropDown, testTypesDropDown));
+        addUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewUserActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/bioauthentication/user/NewUserActivity.java
+++ app/src/main/java/com/example/bioauthentication/user/NewUserActivity.java
@@ -1,16 +1,18 @@
-package com.example.bioauthentication.new_user;
+package com.example.bioauthentication.user;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bioauthentication.R;
+
 public class NewUserActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_new_user);
 
 
     }
